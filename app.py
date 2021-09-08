67e884debbf60034a4c2432869910db05fd803b5
EC2 Default User <ec2-user@ip-172-31-18-128.us-east-2.compute.internal>
2021-09-08 16:47:43 +0000

    added download ability

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import logging
-from flask import Flask, request
+from flask import Flask, request, send_file
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__) 
@@ -22,8 +22,13 @@ def upload_worldmap():
                 return 'Uploading file'
         if request.method == 'GET':
                 return 'get file'
-        
- 
+    
+@app.route('/download', methods = ['GET', 'POST'])    
+def download_worldmap():
+    if request.method == 'GET':
+        return send_file('./worldmap', as_attachment=True)
+
+
 if __name__ == '__main__': 
     app.run(port=5000, debug=True)  
         
